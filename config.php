40c3267c121e16e245c17ab9ef356aa5323606da
hdeuhh <volkwise@gmail.com>
2025-06-09 19:59:02 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+
+if (!defined('BOT_OWNER_NAME'))                      define('BOT_OWNER_NAME', 'Venom');
+if (!defined('BOT_OWNER_USERNAME'))                      define('BOT_OWNER_USERNAME', 'LegendUnion');
+if (!defined('BOT_OWNER_ID'))                      define('BOT_OWNER_ID', '7046999022');
+
+if (!defined('BOT_NAME'))               define('BOT_NAME', 'BETA');
+if (!defined('BOT_USERNAME'))               define('BOT_USERNAME', 'RavenXRobot');
+if (!defined('BOT_TOKEN'))                       define('BOT_TOKEN', '7914451304:AAHrQ-9MjwhaFiD6xN4e-RqqVUSYTKsB2fY');
+if (!defined('BOT_LOGS'))                       define('BOT_LOGS', '-1002762463736');
+if (!defined('BOT_GROUP'))                       define('BOT_GROUP', '-1002762463736');
+
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'admin');
+if (!defined('DB_HOST'))                        define('DB_HOST', 'localhost');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'admin');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', 'admin');
